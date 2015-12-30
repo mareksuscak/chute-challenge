db1ebca207f57356164ec3d979ad09df20a5cac1
Marek Suscak <marek.suscak@gmail.com>
2015-12-30 22:58:09 +0100

    Setup assets folder.

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+var express = require('express');
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var config = require('./webpack.config');
 
-var app = new (require('express'))();
+var app = new express();
 var port = 8000;
 
 var compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.use(webpackHotMiddleware(compiler));
+
+app.use('/assets/images', express.static('assets/images', {maxAge: '200d'}));
 
 app.get("*", function(req, res) {
   res.sendFile(__dirname + '/index.html');
