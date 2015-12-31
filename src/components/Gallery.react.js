e30aefab8a7557705fe3759d843d9fa441080b23
Marek Suscak <marek.suscak@gmail.com>
2015-12-31 15:07:30 +0100

    Refactor gallery markup.

--- src/components/Gallery.react.js
+++ src/components/Gallery.react.js
@@ -1,19 +1,31 @@
 import './Gallery.scss';
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import Image from './Image.react';
+import map from 'lodash/collection/map';
+import AssetSchema from '../schemas/asset';
 
 class Gallery extends Component {
   constructor(props, context) {
     super(props, context);
   }
 
+  renderAssetCollection() {
+    return map(this.props.assets, (asset) =>
+      <Image {...asset} key={asset.id}/>
+    );
+  }
+
   render() {
     return (
       <section className="gallery">
-        <i className="fa fa-twitter" style={{ fontSize: 20 }} />
-        <span style={{ fontSize: 14 }}>This text should be Open Sans</span>
+        {this.renderAssetCollection()}
       </section>
     );
   }
 }
+
+Gallery.propTypes = {
+  assets: PropTypes.arrayOf(PropTypes.shape(AssetSchema)),
+};
 
 export default Gallery;
