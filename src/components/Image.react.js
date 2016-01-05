d776f0089536e159ed1f33bf761ecbc91fd250b2
Marek Suscak <marek.suscak@gmail.com>
2016-01-05 21:21:15 +0100

    Fix the Image's markup.

--- src/components/Image.react.js
+++ src/components/Image.react.js
@@ -1,29 +1,37 @@
 import './Image.scss';
 import React, { Component } from 'react';
 import AssetSchema from '../schemas/asset';
+import classNames from 'classnames';
 
 class Image extends Component {
   constructor(props, context) {
     super(props, context);
   }
 
+  renderServiceIcon() {
+    if (['instagram', 'twitter'].indexOf(this.props.service) >= 0) {
+      const className = classNames('icon', `icon-${this.props.service}`);
+      return <i className={className}/>;
+    }
+  }
+
   render() {
     return (
       <article className="image">
         <header>
-          <a href="https://media.getchute.com/m/12I4gudgjf/c/2500342/315x150">
-            <img src="https://media.getchute.com/m/12I4gudgjf/c/2500342/315x150"/>
+          <a href={this.props.url}>
+            <img src={`${this.props.url}/315x150`}/>
           </a>
         </header>
 
-        <section className="">
-          <img src=""/>
-          <span>{this.props.author.name}</span>
-          <i className="icon icon-twitter"/>
-        </section>
+        <div>
+          <img src={this.props.author.avatarUrl}/>
+          <span>{this.props.author.username}</span>
+          {this.renderServiceIcon()}
+        </div>
 
         <footer>
-          <p>Description goes here.</p>
+          <p>{this.props.caption}</p>
         </footer>
       </article>
     );
