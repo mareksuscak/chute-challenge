2aba6b9d42f4e00e227e162fe9977e6a5af866c9
Marek Suscak <marek.suscak@gmail.com>
2016-01-07 02:44:16 +0100

    If some avatars are missing, replace them with a default icon.

--- src/browser/components/UserInfo.react.js
+++ src/browser/components/UserInfo.react.js
@@ -10,12 +10,22 @@ class UserInfo extends Component {
     service: PropTypes.string.isRequired,
   };
 
+  state = {
+    avatarRemoved: false,
+  }
+
+  handleLoadError() {
+    this.setState({ avatarRemoved: true });
+  }
+
   render() {
     const socialProfileUri = toSocialProfileUri(this.props.username, this.props.service);
     const elementType = socialProfileUri ? 'a' : 'div';
     const props = { className: 'user-info' };
+    const src = this.state.avatarRemoved ? 'assets/image/anonymous.jpg' : this.props.avatarUrl;
+    const key = this.state.avatarRemoved ? 'avatar-static' : 'avatar';
     const children = [
-      <img className="avatar" key="avatar" src={this.props.avatarUrl}/>,
+      <img className="avatar" key={key} src={src} onError={(e) => this.handleLoadError(e)}/>,
       <span className="username" key="username">{this.props.username}</span>,
     ];
 
